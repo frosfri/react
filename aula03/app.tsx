e4c9a0c2ec6b7bb862d342ace9fe578fd87fa7ff
LUIZ <luizhenriquedossantos1505@gmail.com>
2026-02-19 12:24:44 -0300

    Add initial App component with personal information

---
+++ aula03/app.tsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <div id='Container'>
+      <section className='card'>
+        <h1 id='meu-titulo'>Meu Portfólio</h1>
+      </section>
+    
+        <section id='info-pessoal'>
+       <p>luiz henrique dos santos</p>
+        </section>
+<section id='info-email'>
+    <p>E-mail para contato:</p>
+           <p> luizhenriquedossantos1505@gmail.com</p>
+        </section>
+
+        <section id = 'info-git'>
+          <p>GitHub:</p>
+          <p>github.com/frosfri</p>
+        </section>
+
+
+
+      
+
+
+      </div>
+
+
+    
+    </>
+  )
+}
+
+export default App
